4b68896d6e29a54f26e4a6eb5ba84297ebda7be4
Carter Nelms <carternelms@bellsouth.net>
2014-04-09 09:36:33 -0500

    javascript-basics initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// this is a comment
+
+/* this is a
+	multiline comment
+	*/
+
+var firstName = prompt('What is your first name?');
+var lastName = prompt('What is your last name?');
+var fullName = firstName + ' ' + lastName;
+
+var age = prompt('What is your age?');
+
+debugger;
